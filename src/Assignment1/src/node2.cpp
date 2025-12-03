9882f46388ca9406b538600ec679ad615d534f5f
CaroP-unige <carolina.polese@yahoo.it>
2025-12-03 22:23:12 +0000

    Final Update 2.0

--- src/Assignment1/src/node2.cpp
+++ src/Assignment1/src/node2.cpp
@@ -9,98 +9,131 @@
 #include "std_msgs/msg/float32.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
-
 using namespace std::chrono_literals;
 
 class DistanceMonitor : public rclcpp::Node {
 public:
+    /*
+    * Node constructor.
+    *
+    * Initializes subscribers for turtle poses and last user velocity commands,
+    * publishers for distance and corrective commands, and a periodic timer.
+    */
     DistanceMonitor() : Node("node2") {
 
-        // Subscriber to receive the position of turtle1
+        // Subscribers to receive the position of turtle1 and turtle2
         sub1_ = this->create_subscription<turtlesim::msg::Pose>(
             "turtle1/pose", 10,
             std::bind(&DistanceMonitor::pose_callback1, this, std::placeholders::_1)
         );
 
-        // Subscriber to receive the position of turtle2
         sub2_ = this->create_subscription<turtlesim::msg::Pose>(
             "turtle2/pose", 10,
             std::bind(&DistanceMonitor::pose_callback2, this, std::placeholders::_1)
         );
 
+        // Subscribers to receive the last user velocity commands
+        sub_vel1_ = this->create_subscription<geometry_msgs::msg::Twist>(
+            "turtle1/cmd_vel", 10,
+            [this](geometry_msgs::msg::Twist::SharedPtr msg)
+            {
+                prec_vel1_ = *msg;
+            });
+
+        sub_vel2_ = this->create_subscription<geometry_msgs::msg::Twist>(
+            "turtle2/cmd_vel", 10,
+            [this](geometry_msgs::msg::Twist::SharedPtr msg)
+            {
+                prec_vel2_ = *msg;
+            });
+
         // Publisher to publish the distance calculated between the two turtles
         dist_pub_ = this->create_publisher<std_msgs::msg::Float32>("distance", 10);
 
-        // Publisher to send stop or correction commands to turtle1 and turtle2
+        // Publishers to send stop or corrective commands to turtle1 and turtle2
         pub1_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
         pub2_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle2/cmd_vel", 10);
 
-        // Periodic timer to check distance and boundaries
+        // Timer to periodically check distance and boundaries
         timer_ = this->create_wall_timer(100ms, std::bind(&DistanceMonitor::check_distance, this));
     }
 
 private:
+    // Subscribers
     rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr sub1_; 
     rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr sub2_; 
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_vel1_;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_vel2_;
+
+    // Publishers
     rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr dist_pub_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub1_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub2_;
     rclcpp::TimerBase::SharedPtr timer_;
 
-    turtlesim::msg::Pose pose1_; // Current position of turtle1
-    turtlesim::msg::Pose pose2_; // Current position of turtle2
-    bool pose1_ready_ = false; // Flag: true if turtle1's position has been received at least once
-    bool pose2_ready_ = false; // Flag: true if turtle2's position has been received at least once
+    // Last received velocity commands from the user
+    geometry_msgs::msg::Twist prec_vel1_, prec_vel2_;
 
-    const float distance_threshold_ = 1.0; // minimum allowed distance between the turtles
-    const float x_min_ = 1.0, x_max_ = 10.0; // X limits of the workspace
-    const float y_min_ = 1.0, y_max_ = 10.0; // Y limits of the workspace
+    // Current positions of the turtles
+    turtlesim::msg::Pose pose1_;
+    turtlesim::msg::Pose pose2_;
+    bool pose1_ready_ = false;
+    bool pose2_ready_ = false;
 
-    // Callback to update the position of turtle1
+    // Minimum allowed distance between the turtles
+    const float distance_threshold_ = 1.0;
+
+    // Workspace boundaries
+    const float x_min_ = 1.0, x_max_ = 10.0;
+    const float y_min_ = 1.0, y_max_ = 10.0;
+
+    // Callback to update the position of turtle1, calls the boundary check function
     void pose_callback1(const turtlesim::msg::Pose::SharedPtr msg) {
         pose1_ = *msg;
         pose1_ready_ = true;
-        // Border check for turtle1
-        check_boundaries(pose1_, pub1_, "turtle1");
+        check_boundaries(pose1_, prec_vel1_, pub1_, "turtle1");
     }
 
-    // Callback to update the position of turtle2
+    // Callback to update the position of turtle2, calls the boundary check function
     void pose_callback2(const turtlesim::msg::Pose::SharedPtr msg) {
         pose2_ = *msg;
         pose2_ready_ = true;
-        // Border check for turtle2
-        check_boundaries(pose2_, pub2_, "turtle2");
+        check_boundaries(pose2_, prec_vel2_, pub2_, "turtle2");
     }
 
-    // Function to check the position relative to the edges of the Turtlesim window
+    /*
+    * Function to check if the turtle has exceeded the workspace boundaries
+    * if so, send a corrective command using the last user velocity
+    */
     void check_boundaries(const turtlesim::msg::Pose &pose,
+                          const geometry_msgs::msg::Twist &prec_vel,
                           rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub,
                           const std::string &name)
     {
-        geometry_msgs::msg::Twist vel; // speed message to correct the position
-        bool violation = false;
+        geometry_msgs::msg::Twist invert = prec_vel;
 
-        // Check upper and lower limit in X
-        if (pose.x > x_max_) { vel.linear.x = -0.5; violation = true; }
-        else if (pose.x < x_min_) { vel.linear.x = 0.5; violation = true; }
+        if (pose.x < x_min_ || pose.x > x_max_ || pose.y < y_min_ || pose.y > y_max_) {
 
-        // Check upper and lower limit in Y
-        if (pose.y > y_max_) { vel.linear.x = -0.5; violation = true; }
-        else if (pose.y < y_min_) { vel.linear.x = 0.5; violation = true; }
+            // Create command by reversing the user's last speed
+            geometry_msgs::msg::Twist invert = prec_vel;
+            invert.linear.x = -prec_vel.linear.x;
 
-        // If the turtle has exceeded the limits, send corrective command
-        if (violation) {
             RCLCPP_WARN(this->get_logger(), "%s is OUTSIDE the window! Moving back...", name.c_str());
-            pub->publish(vel);
-            rclcpp::sleep_for(500ms); // Waits 0.5 seconds to allow the correction
+            pub->publish(invert);
 
-            // Stop the turtle after the correction
-            vel.linear.x = 0;
-            pub->publish(vel);
+            rclcpp::sleep_for(200ms); // waits half a second for the correction
+
+            // Turtle stop after adjustment
+            invert.linear.x = 0.0;
+            pub->publish(invert);
         }
     }
 
-    // Function to check the distance between the two turtles
+    /*
+    * Function to monitor the distance between the two turtles,
+    * publish the current distance, and apply corrective actions 
+    * if they get too close to each other.
+    */
     void check_distance() {
 
         // Check that both positions are available
